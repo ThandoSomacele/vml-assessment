0624eb059fd9d57886588d6a255f63285fa62da6
Thando Somacele <tsomacele@gmail.com>
2024-03-09 16:45:17 +0200

    acitve states complete: todo transiontion/animations

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,13 +3,14 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { ChevronDownIcon } from '@heroicons/react/20/solid';
+
 
 function Header() {
   const [displayFetures,setDisplyFeature] = useState('hidden');
   const [displayComapany,setDisplyCompany] = useState('hidden');
-  const toggleFeatures = (e)=>{
-   console.log(e.target);
+
+
+  const toggleFeatures = ()=>{
     displayFetures === 'hidden' ? setDisplyFeature('flex') : setDisplyFeature('hidden');
   }
   const toggleCompany = ()=>{
@@ -18,45 +19,43 @@ function Header() {
 
 
   return (
-    <div className='header md:px-10 px-4 md:py-6 py-4 flex justify-between lg:justify-start lg:gap-16 w-screen'>
-      <Image src='/logo.svg' width='84' height='27' alt='snap logo' className='object-contain' />
-      {/* <Bars3Icon className='w-10 lg:hidden' /> */}
-      <Image className='object-contain lg:hidden' src='/icons/icon-menu.svg' width='32' height='32' alt='snap logo' />
+    <div className='header px-10 py-6 lg:flex items-center justify-start gap-16 w-screen hidden' >
+      <Link href={'/'}><Image src='/logo.svg' width='84' height='27' alt='snap logo' className='object-contain' /></Link>
       <nav className='lg:flex items-center justify-between w-full hidden lg:text-sm tracking-[0.5px]'>
         <ul className='flex gap-10 text-medium_gray'>
           <li>
-            <Link href='#' className='flex gap-1.5 items-center' onClick={toggleFeatures}>
-              Features <Image className='object-contain' src='/icons/icon-arrow-down.svg' width={10} height={10} alt='dropdown icon'/>
+            <Link href={'#'} className='group focus:text-almost_black flex gap-1.5 items-center hover:text-almost_black' onClick={toggleFeatures}>
+              Features <Image className='object-contain group-focus:rotate-180' src='/icons/icon-arrow-down.svg' width={10} height={10} alt='dropdown icon'/>
             </Link>
             <ul className={`${displayFetures} bg-white absolute p-6 flex-col gap-3 shadow-2xl rounded-xl -ml-[5%] mt-3.5 text-[13px] z-50`}>
-                <li className='flex items-center gap-4'><Image className='object-contain' src='/icons/icon-todo.svg' width={14} height={14} alt='todo icon'/>Todo List</li>
-                <li className='flex items-center gap-4'><Image className='object-contain' src='/icons/icon-calendar.svg' width={14} height={14} alt='calendar icon'/>Calendar</li>
-                <li className='flex items-center gap-4'><Image className='object-contain' src='/icons/icon-reminders.svg' width={14} height={14} alt='reminders icon'/>Reminders</li>
-                <li className='flex items-center gap-4'><Image className='object-contain' src='/icons/icon-planning.svg' width={14} height={14} alt='planning icon'/>Planning</li>
+                <li><Link className='hover:text-[#726CEE] flex items-center gap-4' href={'#'} onClick={toggleFeatures}><Image className='object-contain' src='/icons/icon-todo.svg' width={14} height={14} alt='todo icon'/>Todo List</Link></li>
+                <li><Link className='hover:text-[#4BB1DA] flex items-center gap-4' href={'#'} onClick={toggleFeatures}><Image className='object-contain' src='/icons/icon-calendar.svg' width={14} height={14} alt='calendar icon'/>Calendar</Link></li>
+                <li><Link className='hover:text-[#EDD556] flex items-center gap-4' href={'#'} onClick={toggleFeatures}><Image className='object-contain' src='/icons/icon-reminders.svg' width={14} height={14} alt='reminders icon'/>Reminders</Link></li>
+                <li><Link className='hover:text-[#8E4CB6] flex items-center gap-4' href={'#'} onClick={toggleFeatures}><Image className='object-contain' src='/icons/icon-planning.svg' width={14} height={14} alt='planning icon'/>Planning</Link></li>
               </ul>
-              <div className={`${displayFetures} w-screen h-screen bg-transparent absolute top-0 right-0 z-40`}  onClick={toggleFeatures}/>
+              <div className={`${displayFetures} w-screen h-screen absolute top-0 right-0`}  onClick={toggleFeatures}/>
           </li>
           <li>
-            <Link href='#' className='flex gap-1.5 items-center' onClick={toggleCompany}>
-              Company <Image className='object-contain' src='/icons/icon-arrow-down.svg' width={10} height={10} alt='dropdown icon'/>
+            <Link href={'#'} className='group focus:text-almost_black flex gap-1.5 items-center hover:text-almost_black' onClick={toggleCompany}>
+              Company <Image className='group-focus:rotate-180 object-contain' src='/icons/icon-arrow-down.svg' width={10} height={10} alt='dropdown icon'/>
             </Link>
-            <ul className={`${displayComapany} absolute p-6  flex-col gap-3 shadow-2xl rounded-xl mt-3.5 text-[13px]`}>
-              <li className='flex items-center gap-4'>History</li>
-              <li className='flex items-center gap-4'>Our Team</li>
-              <li className='flex items-center gap-4'>Blog</li>
+            <ul className={`${displayComapany} absolute p-6 flex-col gap-3 shadow-2xl rounded-xl mt-3.5 text-[13px] bg-white z-50`}>
+              <li><Link className='hover:text-almost_black' href={'#'} onClick={toggleCompany}>History</Link></li>
+              <li><Link className='hover:text-almost_black' href={'#'} onClick={toggleCompany}>Our Team</Link></li>
+              <li><Link className='hover:text-almost_black' href={'#'} onClick={toggleCompany}>Blog</Link></li>
             </ul>
-            <div className={`${displayComapany} w-screen h-screen bg-transparent absolute top-0 right-0 z-40`}  onClick={toggleCompany}/>
+            <div className={`${displayComapany} w-screen h-screen absolute top-0 right-0`}  onClick={toggleCompany}/>
           </li>
           <li>
-            <Link href='#'>Careers</Link>
+            <Link className='hover:text-almost_black' href={'#'}>Careers</Link>
           </li>
           <li>
-            <Link href='#'>About</Link>
+            <Link className='hover:text-almost_black' href={'#'}>About</Link>
           </li>
         </ul>
         <div className='register-btns flex gap-10 items-center text-medium_gray'>
-          <button>Login</button>
-          <button className='border-2 border-medium_gray px-6 py-2.5 rounded-2xl'>Register</button>
+          <button  className='hover:text-almost_black'>Login</button>
+          <button className='border-2 border-medium_gray px-6 py-2.5 rounded-2xl hover:text-almost_black hover:border-almost_black'>Register</button>
         </div>
       </nav>
     </div>
--- src/app/components/SideBarNav.js
+++ src/app/components/SideBarNav.js
@@ -1,36 +1,54 @@
-import React from 'react'
-import { XMarkIcon } from '@heroicons/react/24/outline';
+'use client'
+import React, { createContext, useContext, useState } from 'react';
+// import { XMarkIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
 
+    
 function SideBarNav() {
+    const [showSidebar, setShowSidebar] = useState('translate-x-[100%]');
+    const [showBgFilter, setShowBgFilter] = useState('hidden');
+    const handleSidebarNav =()=>{
+      showSidebar === 'translate-x-[100%]' ? setShowSidebar('translate-x-[0%]') : setShowSidebar('translate-x-[100%]')
+      showBgFilter === 'hidden' ? setShowBgFilter('flex') : setShowBgFilter('hidden')
+        }
+  
   return (
-    <div className='lg:hidden mobile-bg-filter w-screen h-screen bg-black bg-opacity-70 absolute flex justify-end items-start '>
-      <div className='bg-white w-2/3 h-screen px-6 py-4 flex flex-col justify-start items-end overflow-hidden'>
+    <>
+     <div className='mobile-header md:p-8 p-4 lg:hidden flex justify-between  w-screen' >
+       <Link href={'/'}><Image src='/logo.svg' width='84' height='27' alt='snap logo' className='object-contain' /></Link>
+        <Image className='object-contain lg:hidden' src='/icons/icon-menu.svg' width='32' height='32' alt='snap logo' onClick={handleSidebarNav} />
+      </div>
+    <div className={`lg:hidden ${showBgFilter} mobile-bg-filter w-screen h-screen bg-black bg-opacity-70 absolute top-0 right-0 flex justify-end items-start overflow-hidden`} onClick={handleSidebarNav}>
+      <div className={`${showSidebar} bg-white w-2/3 h-screen px-6 py-4 flex flex-col justify-start items-end`}>
             {/* <XMarkIcon className='w-10 text-almost_black' /> */}
-            <Image className='object-contain lg:hidden' src='/icons/icon-close-menu.svg' width='32' height='32' alt='snap logo' />
+            <Image className='object-contain lg:hidden' src='/icons/icon-close-menu.svg' width='32' height='32' alt='snap logo' onClick={handleSidebarNav}/>
             <nav className='lg:hidden w-full text-sm py-5 md:px-10 tracking-[0.5px]'>
               <ul className='flex flex-col gap-6 text-medium_gray'>
                 <li>
-                  <Link href='#' className='flex gap-3 items-center'>
-                    Features <Image className='object-contain' src='/icons/icon-arrow-down.svg' width={11} height={11} alt='dropdown icon'/>
-                  </Link>
+                <details className="group [&_summary::-webkit-details-marker]:hidden">
+                  <summary href='#' className='flex gap-3 items-center'>
+                    Features <Image className='group-open:rotate-180 object-contain' src='/icons/icon-arrow-down.svg' width={11} height={11} alt='dropdown icon'/>
+                  </summary>
                   <ul className='px-5 pt-5 flex flex-col gap-5'>
                     <li className='flex items-center gap-4'><Image className='object-contain' src='/icons/icon-todo.svg' width={16} height={16} alt='todo icon'/>Todo List</li>
                     <li className='flex items-center gap-4'><Image className='object-contain' src='/icons/icon-calendar.svg' width={16} height={16} alt='calendar icon'/>Calendar</li>
                     <li className='flex items-center gap-4'><Image className='object-contain' src='/icons/icon-reminders.svg' width={16} height={16} alt='reminders icon'/>Reminders</li>
                     <li className='flex items-center gap-4'><Image className='object-contain' src='/icons/icon-planning.svg' width={16} height={16} alt='planning icon'/>Planning</li>
                   </ul>
+                  </details>
                 </li>
                 <li>
-                  <Link href='#' className='flex gap-3 items-center'>
-                    Company <Image className='object-contain' src='/icons/icon-arrow-down.svg' width={11} height={11} alt='dropdown icon'/>
-                  </Link>
+                 <details className="group [&_summary::-webkit-details-marker]:hidden">
+                 <summary href='#' className='flex gap-3 items-center'>
+                    Company <Image className='group-open:rotate-180 object-contain' src='/icons/icon-arrow-down.svg' width={11} height={11} alt='dropdown icon'/>
+                  </summary>
                   <ul className='px-5 pt-5 flex flex-col gap-5'>
                     <li className='flex items-center gap-4'>History</li>
                     <li className='flex items-center gap-4'>Our Team</li>
                     <li className='flex items-center gap-4'>Blog</li>
                   </ul>
+                 </details>
                 </li>
                 <li>
                   <Link href='#'>Careers</Link>
@@ -46,6 +64,7 @@ function SideBarNav() {
             </nav>
         </div>
       </div>
+      </>
   )
 }
 
