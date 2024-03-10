6dc95c45073373fd74b7e3ec4f11d4f01fad1490
Thando Somacele <tsomacele@gmail.com>
2024-03-10 22:24:45 +0200

    slow transition expand for sidebar

--- src/app/components/SideBarNav.js
+++ src/app/components/SideBarNav.js
@@ -1,6 +1,5 @@
 'use client'
-import React, { createContext, useContext, useState } from 'react';
-// import { XMarkIcon } from '@heroicons/react/24/outline';
+import React, { useRef, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
@@ -8,48 +7,72 @@ import Image from 'next/image';
 function SideBarNav() {
     const [showSidebar, setShowSidebar] = useState('right-[-100%]');
     const [showBgFilter, setShowBgFilter] = useState('hidden');
-    const handleSidebarNav =()=>{
-      showSidebar === 'right-[-100%]' ? setShowSidebar('right-0') : setShowSidebar('right-[-100%]')
-      showBgFilter === 'hidden' ? setShowBgFilter('flex') : setShowBgFilter('hidden')
-        }
+    const featureContent = useRef()
+    const companyContent = useRef()
+    const [isFeatureCollapsed, setIsFeatureCollapsed] = useState(true);
+    const [isCompanyCollapsed, setIsCompanyCollapsed] = useState(true);
+    const [isFeautureRotated, setIsFeatureRotated] = useState(false);
+    const [isCompanyRotated, setIsCompanyRotated] = useState(false);
+
+    const handleSidebarNav = () => {
+      setShowSidebar(showSidebar === 'right-[-100%]' ? 'right-0' : 'right-[-100%]');
+      setShowBgFilter(showBgFilter === 'hidden' ? 'flex' : 'hidden');
+    };
+
+    function handleFeatureCollapse() {
+      setIsFeatureCollapsed(!isFeatureCollapsed); 
+      setIsFeatureRotated(!isFeautureRotated)
+    }
+
+    function handleCompanyCollapse() {
+        setIsCompanyCollapsed(!isCompanyCollapsed); 
+        setIsCompanyRotated(!isCompanyRotated);
+      }
+    
+    const featureContentStyle = {
+      maxHeight: isFeatureCollapsed ? "0px" : `${featureContent.current?.scrollHeight}px`,
+      overflow: 'hidden',
+      transition: 'max-height 0.3s ease-out',
+    };
+
+    const companyContentStyle = {
+      maxHeight: isCompanyCollapsed ? "0px" : `${companyContent.current?.scrollHeight}px`,
+      overflow: 'hidden',
+      transition: 'max-height 0.3s ease-out',
+    };
+        
   
   return (
     <>
-     <div className='mobile-header md:p-8 p-4 lg:hidden flex justify-between  w-screen' >
+     <div className='mobile-header md:p-8 p-4 lg:hidden flex justify-between w-screen' >
        <Link href={'/'}><Image src='/logo.svg' width='84' height='27' alt='snap logo' className='object-con`tain' /></Link>
         <Image className='object-contain lg:hidden' src='/icons/icon-menu.svg' width='32' height='32' alt='snap logo' onClick={handleSidebarNav} />
       </div>
     <div className={`lg:hidden ${showBgFilter} mobile-bg-filter w-screen h-screen bg-black bg-opacity-70 absolute top-0 right-0 flex justify-end items-start overflow-hidden`} onClick={handleSidebarNav}>
       </div>
       <div className={`fixed top-0 ${showSidebar} flex transition-all duration-1000 bg-white w-2/3 h-screen px-6 py-4 flex-col justify-start items-end`}>
-            {/* <XMarkIcon className='w-10 text-almost_black' /> */}
             <Image className='object-contain lg:hidden' src='/icons/icon-close-menu.svg' width='32' height='32' alt='snap logo' onClick={handleSidebarNav}/>
             <nav className='lg:hidden w-full text-sm py-5 md:px-10 tracking-[0.5px]'>
               <ul className='flex flex-col gap-6 text-medium_gray'>
                 <li>
-                <details className="group [&_summary::-webkit-details-marker]:hidden">
-                  <summary href='#' className='flex gap-3 items-center'>
-                    Features <Image className='group-open:rotate-180 object-contain' src='/icons/icon-arrow-down.svg' width={11} height={11} alt='dropdown icon'/>
-                  </summary>
-                  <ul className='px-5 pt-5 flex flex-col gap-5'>
-                    <li className='flex items-center gap-4'><Image className='object-contain' src='/icons/icon-todo.svg' width={16} height={16} alt='todo icon'/>Todo List</li>
+                <button className='flex gap-3 items-center' onClick={handleFeatureCollapse}>Features <Image className={`${isFeautureRotated ? 'rotate-180' : 'rotate-0'} object-contain`} src='/icons/icon-arrow-down.svg' width={11} height={11} alt='dropdown icon'/>
+                  </button>
+                  <ul className='px-5 flex flex-col gap-5' style={featureContentStyle} ref={featureContent}>
+                    <li className='flex mt-5 items-center gap-4'><Image className='object-contain' src='/icons/icon-todo.svg' width={16} height={16} alt='todo icon'/>Todo List</li>
                     <li className='flex items-center gap-4'><Image className='object-contain' src='/icons/icon-calendar.svg' width={16} height={16} alt='calendar icon'/>Calendar</li>
                     <li className='flex items-center gap-4'><Image className='object-contain' src='/icons/icon-reminders.svg' width={16} height={16} alt='reminders icon'/>Reminders</li>
                     <li className='flex items-center gap-4'><Image className='object-contain' src='/icons/icon-planning.svg' width={16} height={16} alt='planning icon'/>Planning</li>
                   </ul>
-                  </details>
                 </li>
                 <li>
-                 <details className="group [&_summary::-webkit-details-marker]:hidden">
-                 <summary href='#' className='flex gap-3 items-center'>
-                    Company <Image className='group-open:rotate-180 object-contain' src='/icons/icon-arrow-down.svg' width={11} height={11} alt='dropdown icon'/>
-                  </summary>
-                  <ul className='px-5 pt-5 flex flex-col gap-5'>
-                    <li className='flex items-center gap-4'>History</li>
-                    <li className='flex items-center gap-4'>Our Team</li>
+          
+                 <button className='flex gap-3 items-center' onClick={handleCompanyCollapse}>Company<Image className={`${isCompanyRotated ? 'rotate-180' : 'rotate-0'} object-contain`} src='/icons/icon-arrow-down.svg' width={11} height={11} alt='dropdown icon'/>
+                  </button>
+                  <ul className='px-5 flex flex-col gap-5' style={companyContentStyle} ref={companyContent}>
+                    <li className='flex mt-5 items-center gap-4'>History</li>
+                    <li className='flex items-center gap-4'>Our Team</li>ç
                     <li className='flex items-center gap-4'>Blog</li>
                   </ul>
-                 </details>
                 </li>
                 <li>
                   <Link href='#'>Careers</Link>
